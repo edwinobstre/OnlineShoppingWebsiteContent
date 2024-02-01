74f1804d2ace1124ea56db09533588d02889851a
yuleiwan <yuleiwan@amazon.com>
2024-02-01 16:56:57 +0800

    Add IndexController

--- src/main/java/com/yunjiu/OnlineShoppingWebsiteContent/app/OnlineShoppingWebsiteContentApplication.java
+++ src/main/java/com/yunjiu/OnlineShoppingWebsiteContent/app/OnlineShoppingWebsiteContentApplication.java
@@ -1,4 +1,4 @@
-package com.yunjiu.OnlineShoppingWebsiteContent;
+package com.yunjiu.OnlineShoppingWebsiteContent.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
