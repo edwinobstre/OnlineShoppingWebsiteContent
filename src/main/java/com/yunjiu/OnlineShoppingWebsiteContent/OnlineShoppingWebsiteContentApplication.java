f201ae1d8e11f1b18b8696d0f70a7bb37b2e5714
Yulei Wang <yuleiwang@Yuleis-MacBook-Pro.local>
2024-02-08 19:34:38 +0800

    Add dependency and log
    Signed-off-by: Yulei Wang <yuleiwang@Yuleis-MacBook-Pro.local>

--- src/main/java/com/yunjiu/OnlineShoppingWebsiteContent/OnlineShoppingWebsiteContentApplication.java
+++ src/main/java/com/yunjiu/OnlineShoppingWebsiteContent/OnlineShoppingWebsiteContentApplication.java
@@ -1,4 +1,4 @@
-package com.yunjiu.OnlineShoppingWebsiteContent.app;
+package com.yunjiu.OnlineShoppingWebsiteContent;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/main/java/com/yunjiu/OnlineShoppingWebsiteContent/controller/IndexController.java
+++ src/main/java/com/yunjiu/OnlineShoppingWebsiteContent/controller/IndexController.java
@@ -1,20 +1,24 @@
-package com.yunjiu.OnlineShoppingWebsiteContent.app;
+package com.yunjiu.OnlineShoppingWebsiteContent.controller;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
 
-import org.springframework.stereotype.Controller;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 public class IndexController {
+    private static final Logger logger = LogManager.getLogger("HelloWorld");
 
     /**
      * Controller function for the index HTML page.
      */
     @RequestMapping("/")
     public String index(Model model) {
+        logger.info("I am here");
         SimpleDateFormat dateFormat = new SimpleDateFormat("MMM d yyyy : HH:mm:ss:SSS z");
         Date now = new Date();
         String dateStr = dateFormat.format( now );
